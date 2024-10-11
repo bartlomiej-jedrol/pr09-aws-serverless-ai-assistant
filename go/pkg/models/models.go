ea7728d9106f9eb268d180239ec413e09ac43643
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-10-11 12:47:42 +0200

    added Slack Message type with underlying types

--- go/pkg/models/models.go
+++ go/pkg/models/models.go
@@ -7,3 +7,24 @@ type LinkShortenerInputPayload struct {
 type LinkShortenerOutputPayload struct {
 	ShortLink string `json:"shortLink"`
 }
+
+type TextElement struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type LinkElement struct {
+	Type string `json:"type"`
+	URL  string `json:"url"`
+}
+
+type RichTextSection struct {
+	Type     string `json:"type"`
+	Elements []interface{}
+}
+
+type SlackMessage struct {
+	Type     string `json:"type"`
+	BlockID  string `json:"block_id"`
+	Elements RichTextSection
+}
