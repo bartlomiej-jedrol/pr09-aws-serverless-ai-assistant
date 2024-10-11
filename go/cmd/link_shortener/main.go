75ddd28c6f883117ba6ac54e8a1181c22cb8fa22
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-10-11 12:44:06 +0200

    modified references after change of the pkg/types to pkg/models

--- go/cmd/link_shortener/main.go
+++ go/cmd/link_shortener/main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/bartlomiej-jedrol/pr09-aws-serverless-ai-assistant/go/pkg/types"
+	"github.com/bartlomiej-jedrol/pr09-aws-serverless-ai-assistant/go/pkg/models"
 	"github.com/bartlomiej-jedrol/pr09-aws-serverless-ai-assistant/go/pkg/utils"
 	dub "github.com/dubinc/dub-go"
 	"github.com/dubinc/dub-go/models/operations"
@@ -28,23 +28,23 @@ func ShortenLinkWithDub(token string, longLink string) string {
 	return res.ShortLink
 }
 
-func HandleRequest(ctx context.Context, event json.RawMessage) (types.LinkShortenerOutputPayload, error) {
+func HandleRequest(ctx context.Context, event json.RawMessage) (models.LinkShortenerOutputPayload, error) {
 	log.Printf("input from router_lambda to link_shortener lambda: %s", string(event))
 	token := utils.GetEnvVariable("DUB_API_KEY")
 	if token == nil {
 		log.Println("failed to get dub api token")
-		return types.LinkShortenerOutputPayload{}, errors.New("failed to get dub api token")
+		return models.LinkShortenerOutputPayload{}, errors.New("failed to get dub api token")
 	}
 
-	inputPayload := types.LinkShortenerInputPayload{}
+	inputPayload := models.LinkShortenerInputPayload{}
 	err := json.Unmarshal(event, &inputPayload)
 	if err != nil {
 		log.Printf("failed to unmarshal JSON: %s", err)
-		return types.LinkShortenerOutputPayload{}, err
+		return models.LinkShortenerOutputPayload{}, err
 	}
 
 	log.Printf("long link: %s", inputPayload.LongLink)
-	outputPayload := types.LinkShortenerOutputPayload{}
+	outputPayload := models.LinkShortenerOutputPayload{}
 	// longLink := "https://gist.github.com/bartlomiej-jedrol/6c1010ae182de054641608b04eecacfe"
 	outputPayload.ShortLink = ShortenLinkWithDub(*token, inputPayload.LongLink)
 	log.Printf("short link: %s", outputPayload.ShortLink)
